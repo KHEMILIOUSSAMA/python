390001895ebca60dd28b5442e910e1a433d3c0dd
oussama <khemilioussama775@gmail.com>
2021-02-17 00:05:52 +0100

    td2

--- base python.py
+++ base python.py
@@ -10,6 +10,7 @@ print("bravo")
 
 
 # NOMBRE PREMIER
+
 N=int(input("saisir un nombre - ")) 
 etat=True
 for i in range (2,N//2) :
@@ -17,7 +18,9 @@ for i in range (2,N//2) :
         etat=False
         break
 		
+		
 # nombre factorielle using for
+ 
 a= int(input("saisir un nombre"))
 p=1
 for i in range(2,a+1):
@@ -25,3 +28,34 @@ for i in range(2,a+1):
 
 print("le factoriel de ",a,"est",p)
 
+# if 2 exist 
+a = int (input("entrer un entier"))
+isexist=False
+if a>0 :
+  ch= str(a)
+  for i in ch:
+    if i ==2:
+      isexist=True
+      break 
+  if isexist == True :
+    print("contient 2 ")
+  else:
+    print("nombre non contient 2 ") 
+else:
+  print("entrer un entier positif")
+
+# is different int
+a = int (input("entrer un entier"))
+isdifferent=True
+if a>0 :
+  ch= str(a)
+  for i in ch:
+    if ch.count(i) > 1 :
+      isdifferent=False
+      break 
+  if isdifferent123 == True :
+    print("nombre distinct")
+  else:
+    print("nombre non distinct") 
+else:
+  print("entrer un entier positif")
