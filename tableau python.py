c217c0febb8f0fdcd4e2fb50205bfba9c6c7e1c3
oussama <khemilioussama775@gmail.com>
2021-02-18 13:25:35 +0100

    add produits ,somme,nombre pairs

--- tableau python.py
+++ tableau python.py
@@ -45,3 +45,37 @@ def positif(T):
     
 T = [3, 5, 8, -13, -19, 2, 6]
 positif(T)
+
+def somme(T):
+  som =0
+  for i in T:
+   som+=i
+   #la somme des éléments du tableau
+  print ("la somme est",som)
+T = [12, 5, 8, 13, 19, 2, 6]
+somme(T)
+# le produit des éléments du tableau
+def produit (T):
+  prod=1
+  for i in T:
+   prod*=i
+  print ("le produit est",prod)
+T = [1, 5, 2, 2, 1, 1, 1]
+produit(T)
+
+#les nombres pairs
+def pairs(T):
+  for i in T:
+    if i%2==0: print("le nombre pair sont",i)
+T = [12, 5, 8, 13, 19, 2, 6]
+pairs(T)
+      
+     # liste des nombres pairs 
+def pairslist(T):
+  nombrepair=''
+  for i in T:
+    if i%2==0:
+      nombrepair+=str(i)+', '
+  print("la liste des nombres pairs est",nombrepair)
+T = [12, 5, 8, 13, 19, 2, 6]
+pairslist(T)
